1571dc13e83216e800705b7403f674c624b2d833
Teo <mmanchola12temporal@gmail.com>
2026-07-04 13:10:07 +0200

    feat(pwa): configure service worker in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import type { ApplicationConfig } from '@angular/core';
-import { provideBrowserGlobalErrorListeners } from '@angular/core';
+import { isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -10,5 +11,9 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
